142025b566ddb4adbbfdc88439495ee2bebf4ebd
Pedro Carneiro <82902852+pedro-CL@users.noreply.github.com>
2023-10-24 20:24:44 -0300

    Add files via upload

---
+++ Exercise 2/Respostas/1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+main(){
+	
+	int numero, multi;
+	printf("Digite o numero: ");
+	scanf("%d", &numero);
+	
+	multi = numero * 2;
+	
+	printf("\nO dobro do valor digitado: %d", multi);
+	
+}
---
+++ Exercise 2/Respostas/9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+main(){
+	
+	float custo,total;
+	const int DISTRIBUIDOR = 28;
+	const int IMPOSTO = 45;
+	
+	printf("Qual custo inicial do carro: R$");
+	scanf("%f", &custo);
+	
+	total = custo + (custo*DISTIBUIDOR/100) + (custo*IMPOSTO/100);
+	
+	printf("\nCusto toral: %.2f", total);
+	
+}
