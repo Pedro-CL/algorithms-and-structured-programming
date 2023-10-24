43218f4e09882e275895784a2f7f73923021f66e
Pedro Carneiro <82902852+pedro-CL@users.noreply.github.com>
2023-10-24 20:38:21 -0300

    Add files via upload

---
+++ Atividade 2/Atividade 3/Respostas/2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main(){
+    
+    float salario;
+    
+    printf("Digite o salario: ");
+    scanf("%f",&salario);
+    
+    if(idade >= 1320){
+        printf("Salario maior ou igual ao minimo");
+    }
+    else{
+        printf("Salario menor aque o minimo");
+    }
+
---
+++ Atividade 2/Atividade 3/Respostas/4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+main(){
+    
+    double peso;
+    
+    printf("Digite seu peso: ");
+    scanf("%lf",&peso);
+    
+    if(idade >= 60.0){
+        printf("Peso maior que 60");
+    }
+    else{
+        printf("Peso menor que 60");
+    }
+
+}
